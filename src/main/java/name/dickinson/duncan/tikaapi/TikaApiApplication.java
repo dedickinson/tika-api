b5d1df87a6277dd34aa2ad26c4c0b694fe56c57e
Duncan Dickinson <d.dickinson@griffith.edu.au>
2017-11-09 16:12:21 +1000

    Initial import

---
+++ src/main/java/name/dickinson/duncan/tikaapi/TikaApiApplication.java
@@ -0,0 +1,12 @@
+package name.dickinson.duncan.tikaapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TikaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TikaApiApplication.class, args);
+	}
+}
